08ee2b41091c710d75167798ad9c3eee5cd751d1
Agustin Martinez Tambella <73ma37431336@campus.economicas.uba.ar>
2023-06-01 22:40:48 -0300

    Cambios de estructura
    Cambios de estructura

---
+++ HardwareSpringfield/HardwareSpringfield.Consola/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HardwareSpringfield.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+
+            Thread.Sleep(2500);
+        }
+    }
+}
